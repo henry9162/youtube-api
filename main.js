66ee545df3aaa41f090c39944f5ad5a26896c438
henry9162 <henimastic@yahoo.com>
2020-06-15 13:47:18 +0100

    WIP

--- main.js
+++ main.js
@@ -13,74 +13,60 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 const executeBtn = document.getElementById('executeBtn');
+const defaultChannel = 'CreativeH';
 
-const defaultChannel = 'Ekwonwa Henry';
+// function authenticate() {
+//   return gapi.auth2.getAuthInstance()
+//       .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+//       .then(function() { console.log("Sign-in successful"); },
+//             function(err) { console.error("Error signing in", err); });
+// }
+
+// function loadClient() {
+//   gapi.client.setApiKey(API_KEY);
+//   return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+//       .then(function() { console.log("GAPI client loaded for API"); },
+//             function(err) { console.error("Error loading GAPI client for API", err); });
+// }
+
+
+// gapi.load("client:auth2", function() {
+//   gapi.auth2.init({client_id: CLIENT_ID});
+// });
 
 // Form submit and change channel
-// channelForm.addEventListener('submit', e => {
-//   e.preventDefault();
+channelForm.addEventListener('submit', e => {
+  e.preventDefault();
 
-//   const channel = channelInput.value;
+  const channel = channelInput.value;
 
-//   getChannel(channel);
-// });
+  getChannel(channel);
+});
 
 
 
 // Load auth2 library
-// function handleClientLoad() {
-//   gapi.load('client:auth2', initClient);
-// }
-
-// Init API client library and set up sign in listeners
-// function initClient() {
-//   gapi.client
-//     .init({
-//       discoveryDocs: DISCOVERY_DOCS,
-//       clientId: CLIENT_ID,
-//       scope: SCOPES,
-//       apiKey: API_KEY
-//     })
-//     .then(() => {
-//       // Listen for sign in state changes
-//       gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-//       // Handle initial sign in state
-//       updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-//       authorizeButton.onclick = handleAuthClick;
-//       signoutButton.onclick = handleSignoutClick;
-//     });
-// }
-
-// Update UI sign in state changes
-// function updateSigninStatus(isSignedIn) {
-//   if (isSignedIn) {
-//     authorizeButton.style.display = 'none';
-//     signoutButton.style.display = 'block';
-//     content.style.display = 'block';
-//     videoContainer.style.display = 'block';
-//     executeBtn.style.display = 'block'
-//     getChannel(defaultChannel);
-//   } else {
-//     authorizeButton.style.display = 'block';
-//     signoutButton.style.display = 'none';
-//     content.style.display = 'none';
-//     videoContainer.style.display = 'none';
-//     executeBtn.style.display = 'none'
-//   }
-// }
-
-function authenticate() {
-  return gapi.auth2.getAuthInstance()
-      .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
-      .then(function() { console.log("Sign-in successful"); },
-            function(err) { console.error("Error signing in", err); });
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
 }
 
-function loadClient() {
-  gapi.client.setApiKey(API_KEY);
-  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-      .then(function() { console.log("GAPI client loaded for API"); },
-            function(err) { console.error("Error loading GAPI client for API", err); });
+// Init API client library and set up sign in listeners
+function initClient() {
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES
+    })
+    .then(() => {
+      // Listen for sign in state changes
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      // Handle initial sign in state
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    });
 }
 
 // Make sure the client is loaded and sign-in is complete before calling this method.
@@ -109,107 +95,119 @@ function execute() {
     }
   })
     .then(function(response) {
-      // Handle the results here (response.result has the parsed body).
       console.log("Response", response);
     },
     function(err) { console.error("Execute error", err); });
 }
 
-gapi.load("client:auth2", function() {
-  gapi.auth2.init({client_id: CLIENT_ID});
-});
-
+// Update UI sign in state changes
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    content.style.display = 'block';
+    videoContainer.style.display = 'block';
+    executeBtn.style.display = 'block'
+    getChannel(defaultChannel);
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+    content.style.display = 'none';
+    videoContainer.style.display = 'none';
+    executeBtn.style.display = 'none'
+  }
+}
 
 // Handle login
-// function handleAuthClick() {
-//   gapi.auth2.getAuthInstance().signIn();
-// }
+function handleAuthClick() {
+  gapi.auth2.getAuthInstance().signIn();
+}
 
 // Handle logout
-// function handleSignoutClick() {
-//   gapi.auth2.getAuthInstance().signOut();
-// }
+function handleSignoutClick() {
+  gapi.auth2.getAuthInstance().signOut();
+}
 
 // Display channel data
-// function showChannelData(data) {
-//   const channelData = document.getElementById('channel-data');
-//   channelData.innerHTML = data;
-// }
+function showChannelData(data) {
+  const channelData = document.getElementById('channel-data');
+  channelData.innerHTML = data;
+}
 
 // Get channel from API
-// function getChannel(channel) {
-//   gapi.client.youtube.channels
-//     .list({
-//       part: 'snippet,contentDetails,statistics',
-//       forUsername: channel
-//     })
-//     .then(response => {
-//       console.log(response);
-//       const channel = response.result.items[0];
+function getChannel(channel) {
+  gapi.client.youtube.channels
+    .list({
+      part: 'snippet,contentDetails,statistics',
+      forUsername: channel
+    })
+    .then(response => {
+      console.log(response);
+      const channel = response.result.items[0];
 
-//       const output = `
-//         <ul class="collection">
-//           <li class="collection-item">Title: ${channel.snippet.title}</li>
-//           <li class="collection-item">ID: ${channel.id}</li>
-//           <li class="collection-item">Subscribers: ${numberWithCommas(
-//             channel.statistics.subscriberCount
-//           )}</li>
-//           <li class="collection-item">Views: ${numberWithCommas(
-//             channel.statistics.viewCount
-//           )}</li>
-//           <li class="collection-item">Videos: ${numberWithCommas(
-//             channel.statistics.videoCount
-//           )}</li>
-//         </ul>
-//         <p>${channel.snippet.description}</p>
-//         <hr>
-//         <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
-//           channel.snippet.customUrl
-//         }">Visit Channel</a>
-//       `;
-//       showChannelData(output);
+      const output = `
+        <ul class="collection">
+          <li class="collection-item">Title: ${channel.snippet.title}</li>
+          <li class="collection-item">ID: ${channel.id}</li>
+          <li class="collection-item">Subscribers: ${numberWithCommas(
+            channel.statistics.subscriberCount
+          )}</li>
+          <li class="collection-item">Views: ${numberWithCommas(
+            channel.statistics.viewCount
+          )}</li>
+          <li class="collection-item">Videos: ${numberWithCommas(
+            channel.statistics.videoCount
+          )}</li>
+        </ul>
+        <p>${channel.snippet.description}</p>
+        <hr>
+        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+          channel.snippet.customUrl
+        }">Visit Channel</a>
+      `;
+      showChannelData(output);
 
-//       const playlistId = channel.contentDetails.relatedPlaylists.uploads;
-//       requestVideoPlaylist(playlistId);
-//     })
-//     .catch(err => alert('No Channel By That Name'));
-// }
+      const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+      requestVideoPlaylist(playlistId);
+    })
+    .catch(err => alert('No Channel By That Name'));
+}
 
 // Add commas to number
-// function numberWithCommas(x) {
-//   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-// }
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
-// function requestVideoPlaylist(playlistId) {
-//   const requestOptions = {
-//     playlistId: playlistId,
-//     part: 'snippet',
-//     maxResults: 10
-//   };
+function requestVideoPlaylist(playlistId) {
+  const requestOptions = {
+    playlistId: playlistId,
+    part: 'snippet',
+    maxResults: 10
+  };
 
-//   const request = gapi.client.youtube.playlistItems.list(requestOptions);
+  const request = gapi.client.youtube.playlistItems.list(requestOptions);
 
-//   request.execute(response => {
-//     console.log(response);
-//     const playListItems = response.result.items;
-//     if (playListItems) {
-//       let output = '<br><h4 class="center-align">Latest Videos</h4>';
+  request.execute(response => {
+    console.log(response);
+    const playListItems = response.result.items;
+    if (playListItems) {
+      let output = '<br><h4 class="center-align">Latest Videos</h4>';
 
-//       // Loop through videos and append output
-//       playListItems.forEach(item => {
-//         const videoId = item.snippet.resourceId.videoId;
+      // Loop through videos and append output
+      playListItems.forEach(item => {
+        const videoId = item.snippet.resourceId.videoId;
 
-//         output += `
-//           <div class="col s3">
-//           <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-//           </div>
-//         `;
-//       });
+        output += `
+          <div class="col s3">
+          <iframe width="100%" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </div>
+        `;
+      });
 
-//       // Output videos
-//       videoContainer.innerHTML = output;
-//     } else {
-//       videoContainer.innerHTML = 'No Uploaded Videos';
-//     }
-//   });
-// }
+    // Output videos
+      videoContainer.innerHTML = output;
+    } else {
+      videoContainer.innerHTML = 'No Uploaded Videos';
+    }
+  });
+}
